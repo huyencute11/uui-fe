69a61dccb76ec675636165d11fb74abeed573e8e
Huyen Tran <ttminhuyen1111@gmail.com>
2024-07-09 23:24:11 +0700

    aaa

--- src/components/route/ProtectedRoute.tsx
+++ src/components/route/ProtectedRoute.tsx
@@ -1,5 +1,3 @@
-import appConfig from '@/configs/app.config'
-import { REDIRECT_URL_KEY } from '@/constants/app.constant'
 import {  Outlet} from 'react-router-dom'
 
 const ProtectedRoute = () => {
--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -2,7 +2,6 @@ export type AppConfig = {
     apiPrefix: string
     authenticatedEntryPath: string
     unAuthenticatedEntryPath: string
-    // tourPath: string
     locale: string
     enableMock: boolean
 }
@@ -11,7 +10,6 @@ const appConfig: AppConfig = {
     apiPrefix: '/api',
     authenticatedEntryPath: '/app/users/user-list',
     unAuthenticatedEntryPath: '/app/users/user-list',
-    // tourPath: '/app/account/kyc-form',
     locale: 'en',
     enableMock: true,
 }
